c79d8ec3486ff8aca8d47db77b745e09c8031caf
Filipe Santos <filipssant@gmail.com>
2023-07-10 20:03:05 -0300

    create table only if not exists

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,8 +1,9 @@
 CREATE DATABASE mycontacts;
 
-CREATE EXTENSION "uuid-ossp"
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE categories (
+CREATE TABLE IF NOT EXISTS categories (
   id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
   name VARCHAR NOT NULL
 );
+
